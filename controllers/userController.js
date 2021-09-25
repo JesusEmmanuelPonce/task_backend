213cb30768f884a08cf834f272a009563c21ed97
Jesus Emmanuel Ponce Becerra <j.emmanuel.ponce.b@gmail.com>
2021-09-24 19:40:30 -0500

    user create

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,37 @@
-exports.userCreate = (req, res) => {
-    console.log("Creando...");
+const User = require("../models/user")
+
+exports.userCreate = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const user = new User(req.body);
+        
+        const isUser = await User.findOne({ email });
+
+        if(!isUser) {
+
+            await user.save();
+    
+            res.status(200).json({
+                success: true,
+                msg: `${user.name} se ha agregado correctamente`,
+            });
+
+        } else {
+            res.status(400).json({
+                success: false,
+                msg: `${isUser.name} ya se encuentra registrado`,
+            });
+        }
+        
+
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            msg: "Error en el servidor",
+        })
+    }
 }
